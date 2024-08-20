523d5c3174b218e658baef6814fe4e60f87ea6fd
Prateek <Prateekrajgrd74@gmail.com>
2024-08-20 15:58:37 +0530

    react.html

--- array.js
+++ array.js
@@ -14,3 +14,18 @@ console.log(all_heros);
 // or
 const all_new_heros = [...marvel_heros, ...dc_heros];   // to add two arrays
 console.log(all_new_heros);
+
+
+// Array of
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+// copy within
+
+let arr2 = [1,2,3,4,5,6,7,8,9];
+arr2.copyWithin(1,3,5);
+console.log(arr2);
