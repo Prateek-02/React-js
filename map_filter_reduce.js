e76377be3b3c3c50a5537d88965b199dec3ccaea
Prateek <Prateekrajgrd74@gmail.com>
2024-08-22 16:08:15 +0530

    array.js

--- map_filter_reduce.js
+++ map_filter_reduce.js
@@ -1,3 +1,14 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10];
-const newNums = myNums.map( (num) => num+10);
-console.log(newNums);
+
+// map
+const newNums1 = myNums.map( (num) => num+10);
+console.log(newNums1);
+
+// reduce
+const newNums2 = myNums.reduce((acc,curr) => acc+curr,0)
+console.log(newNums2);
+
+
+// filter
+const newNums3 = myNums.filter((num) => num>5);
+console.log(newNums3);
