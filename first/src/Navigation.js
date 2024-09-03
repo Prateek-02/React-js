0fbd94fa822f29661e59dfa7357f70fac75e7c26
Prateek <Prateekrajgrd74@gmail.com>
2024-09-03 15:57:15 +0530

    first.js

---
+++ first/src/Navigation.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Navigation()
+{
+  return(
+    <div className = "nav">
+        <ul className="nav-links">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/services">Services</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
--- first/src/index.js
+++ first/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navigation from './Navigation';
 ReactDOM.render(
-    <div>
-    <h1>Heading 1</h1>
-    <p3>Paragraph 3</p3>
-    <p>Hello</p>
+  <div>
+      <Navigation/>
+      <h1>Heading 1</h1>
+      <p>Paragraph 3</p>
+      <p>Hello</p>
     </div>,
     document.getElementById("root")
 );
 
 
 // without JSX
-var h1 = document.createElement("h1");
-h1.innerHTML = "Hello World";
-document.getElementById("root").appendChild(h1);
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World";
+// document.getElementById("root").appendChild(h1);
 
 
